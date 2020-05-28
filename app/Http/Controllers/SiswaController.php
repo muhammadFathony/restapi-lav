4c52d6ddec4c7e1d5cc0be22f8463812e87c4d76
muhammadFathony <mfathony115@gmail.com>
2020-05-28 08:10:42 +0700

    first restapi lav

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+
+        return Siswa::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        //
+
+        $siswa = new Siswa;
+        $siswa->nama = $request->nama;
+        $siswa->alamat = $request->alamat;
+        $siswa->save();
+
+        return "Data Berhasil Masuk";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Siswa $siswa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Siswa $siswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+
+        $nama = $request->nama;
+        $alamat = $request->alamat;
+
+        $siswa = Siswa::find($id);
+        $siswa->nama = $nama;
+        $siswa->alamat = $alamat;
+        $siswa->save();
+
+        return "Berhasil di update";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+
+        $siswa = Siswa::find($id);
+        $siswa->delete();
+
+        return "Berhasil di hapus";
+    }
+
+    public function delete($id)
+    {
+        //
+
+        $siswa = Siswa::find($id);
+        $siswa->delete();
+
+        return "Berhasil di hapus";
+    }
+}
